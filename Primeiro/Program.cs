23fe4e48885d8e399ce4a4176a1e6c77ec43a830
Giovanna <Giovanna@DELL>
2021-07-19 15:28:19 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Hello ";
+            var b = "World!";
+            Console.WriteLine(a + b);
+        }
+    }
+}
