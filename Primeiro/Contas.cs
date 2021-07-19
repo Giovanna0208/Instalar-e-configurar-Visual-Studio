fc5fe39f59188407144398ea4b19bae276a42d43
Giovanna Ramos <gramos@essence-rh.com.br>
2021-07-19 17:09:22 -0300

    Testes

---
+++ Primeiro/Contas.cs
@@ -0,0 +1,22 @@
+﻿namespace Contas
+{
+    public class Contas
+    {
+        public int Soma(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtracao(int a, int b)
+        {
+            return a - b;
+        }
+        public int Divisao(int a, int b)
+        {
+            return a / b;
+        }
+        public int Multiplicacao(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ PrimeiroTests/ContasTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Contas.Tests
+{
+    [TestClass()]
+    public class ContasTests
+    {
+        [TestMethod()]
+        public void SomaTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 10;
+            var contas = new Contas();
+
+            var result = contas.Soma(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void SubtracaoTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 0;
+            var contas = new Contas();
+
+            var result = contas.Subtracao(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void DivisaoTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 1;
+            var contas = new Contas();
+
+            var result = contas.Divisao(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void MultiplicacaoTest()
+        {
+            var a = 5;
+            var b = 5;
+            var esperado = 25;
+            var contas = new Contas();
+
+            var result = contas.Multiplicacao(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+    }
+}
